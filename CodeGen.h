26825f8302db0ae794518475d74207d961f25cba
Prashant Pandey <prashant.prashn@gmail.com>
2014-04-25 04:03:47 -0400

    Added while/break test file. Added code gen class.

---
+++ CodeGen.h
@@ -0,0 +1,75 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  CodeGen.h
+ *
+ *    Description:  File containing classes required for final code generation.
+ *
+ *        Version:  1.0
+ *        Created:  04/25/2014 03:19:48 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Prashant Pandey (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
+class FinalCode {
+    
+    FinalCode(string progName) {
+	progName_ = progName;
+	instructions_ = new vector<Instruction>;
+    }
+    
+    void insertInstruction(Instruction *instr) {
+	instructions_->push_back(instr);
+    }
+
+    private:
+	string progName_;
+	vector<Instruction*> *instructions_;
+}
+
+class Instruction {
+    enum InstructionSet {
+	ADD, SUB, DIV, MUL, MOD, FADD, FSUB, FDIV, FMUL, 
+	AND, OR, XOR, NEG, FNEG, 
+	UGT, UGE, GT, GE, EQ, NE, FGT, FGE, FEQ, FNE,
+	PRTS, PRTI, PRTF,
+	JMP, JMPC, JMPI, JMPCI,
+	MOVL, MOVS, MOVI, MOVF, MOVIF, MOVFI,
+	STI, STF, LDI, LDF,
+	IN, INI, INF
+    }
+
+    enum ParamType {
+	REGISTER,
+	MEMORY_LOC,
+	VALUE
+    }
+
+    Instruction(InstructionSet instr, string param1, string param2 = "", string param3 = "", string label = "") {
+	instr_ = instr;
+	param1_ = param1;
+	param2_ = param2;
+	param3_ = param3;
+	label_ = label;
+    }
+
+    InstructionSet getInstr() { return instr_; }
+    string getParam1() { return param1_; }
+    string getParam2() { return param2_; }
+    string getParam3() { return param3_; }
+    string getLabel() { return label_; }
+     
+    private:
+	InstructionSet instr_;
+	string param1_;
+	string param2_;
+	string param3_;
+	string label_;
+}
+
