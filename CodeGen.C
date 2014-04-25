e7cc48cb247c36ecfd363b3fc5f08e8a1729308d
Prashant Pandey <prashant.prashn@gmail.com>
2014-04-25 05:45:49 -0400

    Added comment for reg allocation and code gen

---
+++ CodeGen.C
@@ -0,0 +1,41 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  CodeGen.C
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04/25/2014 04:05:40 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  , 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <CodeGen.h>
+
+//  Add  book keeping for the special puspose registers
+//  Stack pointer: R999
+//  Base Point: R998
+//  Return Value: R997/F997
+//  Global Vars Base Addr: R996/F996
+//
+//  General Purpose Registers:	returned by the global memory allocation function
+//	two values: one  each for the Integer/Float registers
+//
+//  Register free list for tracking the registers for the temporary vars
+//	: allocate from a global pool of registers
+//	: add the register to used free list when not in use
+//
+//
+//	functions required:
+//	    : string fetchNextAvailReg()
+//	    : void purgeReg(string regName)
+//
+//
+//  Code Gen:
+//	
