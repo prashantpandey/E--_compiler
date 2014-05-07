a08d63da2488c8a1f1828d8b87a11dc37eb6dd59
Ankur <ankurmit2006@gmail.com>
2014-05-07 02:55:29 -0400

    Fixing function exit codegen.

--- STEClasses.C
+++ STEClasses.C
@@ -186,9 +186,10 @@ vector<Instruction*>* FunctionEntry::codeGen() {
     vector<Instruction*> *tmp = body()->codeGen();
     inst_vec->insert(inst_vec->end(), tmp->begin(), tmp->end());
     inst_vec->push_back(new Instruction(Instruction::InstructionSet::MOVI, BP_REG, SP_REG, "", "" ,"Function Exit: Restoring BP"));
-    inst_vec->push_back(new Instruction(Instruction::InstructionSet::LDI, SP_REG, BP_REG));
     inst_vec->push_back(new Instruction(Instruction::InstructionSet::ADD, SP_REG, "1", SP_REG));
-    inst_vec->push_back(new Instruction(Instruction::InstructionSet::LDI, SP_REG, RET_ADDR_REG, "", "", "Getting Return Address"));
+    inst_vec->push_back(new Instruction(Instruction::InstructionSet::LDI, SP_REG, BP_REG, "", "", "Loading BP from stack"));
+    inst_vec->push_back(new Instruction(Instruction::InstructionSet::ADD, SP_REG, "1", SP_REG));
+    inst_vec->push_back(new Instruction(Instruction::InstructionSet::LDI, SP_REG, RET_ADDR_REG, "", "", "Getting Return Address from stack"));
     inst_vec->push_back(new Instruction(Instruction::InstructionSet::ADD, SP_REG, to_string(getArgCnt()), SP_REG));
     inst_vec->push_back(new Instruction(Instruction::InstructionSet::JMPI, RET_ADDR_REG));
     return inst_vec;
